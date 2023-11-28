626cb497743e1d2139e803664b1c7bb132e047ca
Umesh Kumar <103483751+Umeshboby@users.noreply.github.com>
2023-11-28 11:56:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 const arr = [1, 2, 3, 4, 1, 0, 2, 2];
 
 const divide = (arr, n) => {
-  // Write your code here
+  // Write your code here 
+	let i=0 , ans=[];
+	while(i<=arr.length-1){
+		ans.push(arr.slice(i,i+2));
+		i=i+n;
+	}
+	return ans;
 };
 
 const n = prompt("Enter n: ");
