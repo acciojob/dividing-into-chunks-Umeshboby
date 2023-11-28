66389c3cc7c6ef25860291ff3479d44bccab65ed
Umesh Kumar <103483751+Umeshboby@users.noreply.github.com>
2023-11-28 12:06:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 const arr = [1, 2, 3, 4, 1, 0, 2, 2];
 
 const divide = (arr, n) => {
-  // Write your code here 
+  // Write your code here
+	let myInteger = parseInt(n);
 	let i=0 , ans=[];
 	while(i<=arr.length-1){
 		ans.push(arr.slice(i,i+2));
-		i=i+n;
+		i=i+myInteger;
 	}
 	return ans;
 };
 
-const n = prompt("Enter n: ");
-alert(JSON.stringify(divide(arr, n)));
+// const n = prompt("Enter n: ");
+// alert(JSON.stringify(divide(arr, n)));
